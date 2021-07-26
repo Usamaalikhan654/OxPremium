71e0716fbf0732bcd45e72e19f83e217fd9db399
Usamaalikhan654 <84439703+Usamaalikhan654@users.noreply.github.com>
2021-07-26 11:52:38 +0500

    version 4.8
    changes in images

--- src/Pages/Training and consultancies/Training.js
+++ src/Pages/Training and consultancies/Training.js
@@ -1,6 +1,6 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import photo1 from "../../Assets/web opt 2-30 1.png"
 
  function Training () {
       return (
@@ -19,7 +19,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
                   Whether it is about the safety of your staff, your equipment or the services you deliver, we have a highly skilled squad of trainers who not only develop a customized training and workshop for your staff’s safety but also design manual to keep your equipment and processes safe. We also offer safety consultancies to Our clients to help them equip in handling the challenges they face..</p>
               </div>
               <div className="col-lg-6 page-image">
-                <img src="./images/web opt 2-30 1.png" className="img-fluid" width="400px" height="300px" />
+                <img src={photo1} className="img-fluid" width="400px" height="300px" />
               </div>
             </div>
           </div>
