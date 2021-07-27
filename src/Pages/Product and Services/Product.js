4cdc3820fbf5074072f6df273b19baedf690ea37
Usamaalikhan654 <84439703+Usamaalikhan654@users.noreply.github.com>
2021-07-27 14:18:12 +0500

    version 8.4
    changes in the heading of product

--- src/Pages/Product and Services/Product.js
+++ src/Pages/Product and Services/Product.js
@@ -15,7 +15,7 @@ const Product = () => {
     <Jumbotron fluid className="header-pic">
   <Container className="text-center">
     <h1 className="head-1">PRODUCTS AND SERVICES</h1>
-    <p className="head-1">
+    <p className="head-2">
     Manufacturing Comfort. Delivering Choice.
     </p>
     <p className="para-1">
