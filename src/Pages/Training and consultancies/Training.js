f9c8878cfc9e48bf3c299e2915e1533e66ae369b
Usamaalikhan654 <84439703+Usamaalikhan654@users.noreply.github.com>
2021-07-27 10:40:16 +0500

    version 7.5
    working on backgrounds

--- src/Pages/Training and consultancies/Training.js
+++ src/Pages/Training and consultancies/Training.js
@@ -5,7 +5,7 @@ import "./Training.css"
 
  function Training () {
       return (
-        <div>
+        <div className="contain-1">
           <div className="container">
             <div className="main-heading row">
               <div className="col-lg-6 text-center">
@@ -16,8 +16,8 @@ import "./Training.css"
             <hr className="hr-line" />
             <div className="row Fi-region">
               <div className="col-lg-6 page-text">
-                <p>Safety is the core concern of all the responsible organizations and institutions and we understand the lack of available trainings and facilitation for it. This requirement made us work on a full-scale safety trainings and consultancies so that we furnish your need and resolve it fully with our local and cultural knowledge and limitations.<br /><br />
-                  Whether it is about the safety of your staff, your equipment or the services you deliver, we have a highly skilled squad of trainers who not only develop a customized training and workshop for your staff’s safety but also design manual to keep your equipment and processes safe. We also offer safety consultancies to Our clients to help them equip in handling the challenges they face..</p>
+                <p>Safety is the core concern of all the responsible organizations and institutions and we understand the lack of available trainings and facilitation for it. This requirement made us work on a full-scale safety trainings and <br/><br/> consultancies so that we furnish your need and resolve it fully with our local and cultural knowledge and limitations. 
+                   Whether it is about the safety of your staff, your equipment or the services you deliver, we have a highly skilled squad of trainers who not only develop a customized training and workshop for your staff’s safety but also design manual to keep your equipment and processes safe. We also offer safety consultancies to our clients to help them equip in handling the challenges they face. </p>
               </div>
               <div className="col-lg-6 page-image">
                 <img src={photo1} className="img-fluid" width="400px" height="300px" />
